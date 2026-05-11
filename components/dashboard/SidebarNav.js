e570e6e8f0298e780c7e32fe2bc4ddd906ccddc9
Levi Batty <levibatty@Levis-MacBook-Air.local>
2026-05-11 21:23:08 +0100

    Add Messages link to employer master menu

--- components/dashboard/SidebarNav.js
+++ components/dashboard/SidebarNav.js
@@ -114,6 +114,7 @@ function getItems(role) {
       { type: 'link', href: '/employer/post-job', label: 'Post Job', icon: PlusIcon },
       { type: 'link', href: '/employer/search-candidates', label: 'Search Candidates', icon: SearchIcon },
       { type: 'link', href: '/employer/interviews', label: 'Interviews', icon: InterviewsIcon },
+      { type: 'link', href: '/employer/messages', label: 'Messages', icon: MessagesIcon, badge: 'messages' },
       { type: 'link', href: '/employer/offers', label: 'Offers', icon: OffersIcon },
       { type: 'link', href: '/help', label: 'Help', icon: HelpIcon },
       { type: 'divider' },
